871dd99bc024877498d771e510ea6f100cdb46d4
Bruno Almeida <bruno-ralmeida@outlook.com>
2021-05-22 12:32:49 -0300

    add game ref

--- src/repository/concepts/concepts.service.ts
+++ src/repository/concepts/concepts.service.ts
@@ -3,10 +3,11 @@ import data from './concepts';
 export class ConceptsService {
   private readonly base = data;
 
-  fetchContentFromConceptsBase(content: string): string {
-    const result = this.base.find((item) =>
-      item.key.toLowerCase().includes(content),
+  fetchContentFromConceptsBase(content: string) {
+    const result = this.base.find(
+      (item) => item.key.toLowerCase().includes(content) || '',
     );
+
     return result.value || '';
   }
 }
--- src/repository/concepts/concepts.ts
+++ src/repository/concepts/concepts.ts
@@ -16,23 +16,23 @@ const data: DataFormat[] = [
     value:
       'As APIs são um conjunto de padrões que fazem parte de uma interface e que permitem a criação de plataformas de maneira mais simples e prática para desenvolvedores. A partir de APIs é possível criar softwares, aplicativos, programas e plataformas diversas. Por exemplo, apps desenvolvidos para celulares Android e iPhone (iOS) são criados a partir de padrões definidos e disponibilizados pelas APIs de cada sistema operacional.',
   },
-  // {
-  //   key: 'Variável',
-  //   value:
-  //     'Na programação, uma variável é um objeto capaz de reter e representar um valor ou expressão. Enquanto as variáveis só "existem" em tempo de execução, elas são associadas a "nomes", chamados identificadores, durante o tempo de desenvolvimento.',
-  // },
   {
-    key: 'Estrutura de seleção (if / se)',
+    key: 'Variável',
+    value:
+      'Na programação, uma variável é um objeto capaz de reter e representar um valor ou expressão. Enquanto as variáveis só "existem" em tempo de execução, elas são associadas a "nomes", chamados identificadores, durante o tempo de desenvolvimento.',
+  },
+  {
+    key: 'Estrutura de seleção ( if / se )',
     value:
       'Estrutura de seleção é, na ciência da computação, uma estrutura de desvio do fluxo de controle presente em linguagens de programação que realiza diferentes computações ou ações dependendo se a seleção é verdadeira ou falsa, em que a expressão é processada e transformada em um valor booleano.',
   },
   {
-    key: 'Estrutura de repetição (loop / for / while / laço)',
+    key: 'Estrutura de repetição ( loop / for / while / laço )',
     value:
       'Na maioria das linguagens de programação de computadores, um loop while é uma instrução de fluxo de controle que permite que o código seja executado repetidamente com base em uma determinada condição booleana. O loop while pode ser considerado uma declaração if de repetição.',
   },
   {
-    key: 'Array (Vetor / Arranjo)',
+    key: 'Array ( Vetor / Arranjo )',
     value:
       'Em programação de computadores, um arranjo (array) é uma estrutura de dados que armazena uma coleção de elementos de tal forma que cada um dos elementos possa ser identificado por, pelo menos, um índice ou uma chave.',
   },
